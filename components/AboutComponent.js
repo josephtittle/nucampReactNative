1d84b70b11d5194db1068fa71251a5ff3a8b60e6
Joseph Tittle <josephtittle91@gmail.com>
2021-05-15 14:05:26 -0500

    task 3

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -1,19 +1,64 @@
+import { PARTNERS } from '../shared/partners';
 import React, { Component } from 'react';
-import { ScrollView } from 'react-native';
+import { Card, ListItem } from 'react-native-elements';
+import { Text, ScrollView, FlatList } from 'react-native';
 
+class Mission extends Component {
+    render() {
+        return (
+            <Card title= "Our Mission">
+                <Text style={{ margin: 10 }}>
+                    We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
+                </Text>
+            </Card>
+        )
+    }
+
+}
 
 class About extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            partners: PARTNERS
+        }
+    }
+
     static navigationOptions = {
-        title: 'ABout Us'
+        title: 'About Us'
     }
 
     render() {
+        const renderPartners = ({ item }) => {
+            return (
+                <ListItem
+                    title={item.name}
+                    subtitle={item.description}
+                    leftAvatar={{ source: require('./images/bootstrap-logo.png') }}
+                />
+            );
+        };
+
         return (
             <ScrollView>
-                
+                <Mission />
+                <Card title={"Community Partners"}>
+                    <Text style={{
+                        margin: 10,
+                        fontWeight: 'bold', textAlign: 'center', marginBottom: 10
+                    }}>
+                        Community Partners
+                    </Text>
+                    <FlatList
+                        data={this.state.partners}
+                        renderItem={renderPartners}
+                        keyExtractor={item => item.id.toString()} />
+                </Card>
             </ScrollView>
         );
     }
 }
+
+
 
 export default About;
