a58065691b3b1f1018bb8ce20e05fc0b34e2fe9a
Joseph Tittle <josephtittle91@gmail.com>
2021-05-15 12:07:12 -0500

    saturday workshop task 1

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import Home from './HomeComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
 import { View, Platform } from 'react-native';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer } from 'react-navigation';
+
+
 
 const DirectoryNavigator = createStackNavigator(
     {
@@ -26,6 +30,8 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
+
+
 const HomeNavigator = createStackNavigator(
     {
         Home: { screen: Home }
@@ -43,10 +49,48 @@ const HomeNavigator = createStackNavigator(
     }
 );
 
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: { screen: Contact }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
+
+const AboutNavigator = createStackNavigator(
+    {
+        About: { screen: About }
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+        }
+    }
+);
+
 const MainNavigator = createDrawerNavigator(
     {
         Home: { screen: HomeNavigator },
-        Directory: { screen: DirectoryNavigator }
+        Directory: { screen: DirectoryNavigator },
+        About: { screen: AboutNavigator },
+        Contact: { screen: ContactNavigator }
     },
     {
         drawerBackgroundColor: '#CEC8FF'
